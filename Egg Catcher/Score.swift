f7989b2f18813e6df0af4680defa32258bcc6950
dhillon1 <simrandhillon5486@gmail.com>
2020-02-16 22:28:38 -0500

    Released version

--- Egg Catcher/Score.swift
+++ Egg Catcher/Score.swift
@@ -2,7 +2,7 @@
 //  Score.swift
 //  Egg Catcher
 //  Project name = Egg Catcher
-//  Developed by = Harmandeep Kaur (30109) and Simranjeet Singh Dhillon (301093914)
+//  Developed by = Harmandeep Kaur (301092579) and Simranjeet Singh Dhillon (301093914)
 //  Description = Score class
 //  Date modified = 16-02-2020
 //  Copyright © 2020 centennialcollege. All rights reserved.
